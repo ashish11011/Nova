ddde9fa14f26f071f11747efc35db19d28b7dd89
Manu Arora <manuarorawork@gmail.com>
2023-06-13 00:39:26 +0530

    🔧 Fix: JSON parse express

--- public/script.js
+++ public/script.js
@@ -1,65 +1,82 @@
-const lowerElement = document.querySelectorAll('.lower'); 
-const editButton = document.querySelectorAll('.edit');
-for(let i = 0; i < editButton.length; i++) {
-    editButton[i].addEventListener('click', () => {
-        lowerElement[i].classList.toggle('hidden');
-        if(lowerElement[i].classList.contains('hidden')){
-            editButton[i].innerHTML = 'Edit';
-        }else{
-            editButton[i].innerHTML = 'save';
-        }
+const lowerElement = document.querySelectorAll(".lower");
+const editButton = document.querySelectorAll(".edit");
+for (let i = 0; i < editButton.length; i++) {
+  editButton[i].addEventListener("click", () => {
+    lowerElement[i].classList.toggle("hidden");
+    if (lowerElement[i].classList.contains("hidden")) {
+      editButton[i].innerHTML = "Edit";
+    } else {
+      editButton[i].innerHTML = "save";
+    }
 
-        if(editButton[i].innerHTML == 'save'){
-            editButton[i].addEventListener('click', () => {
-                const form = document.querySelector('#list-des-form');
-                form.submit();
-            });
-        }
+    if (editButton[i].innerHTML == "save") {
+      editButton[i].addEventListener("click", () => {
+        const form = document.querySelector("#list-des-form");
+
+        // FIXME: required form.submit()?
+        // form.submit();
+
+        const formDetails = new FormData(form);
+        const des = formDetails.get("des");
+        const count = formDetails.get("count");
+        console.log(des, count);
+
+        // Directly calling the API here
+        fetch("/click", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            des,
+            count,
+          }),
+        })
+          .then((response) => {
+            if (response.ok) {
+              console.log("POST request sent successfully!");
+              // Do your routing either here or in /click route
+            } else {
+              console.error("Error sending POST request:", response.status);
+            }
+          })
+          .catch((error) => {
+            console.error("Error sending POST request:", error);
+          });
+      });
+    }
+  });
+
+  ///////////////////////////
+
+  // Get the H1 element
+  const fiberHeading = document.querySelectorAll(".fiber-heading");
+
+  // Add a click event listener to the H1
+  for (let i = 0; i < fiberHeading.length; i++) {
+    fiberHeading[i].addEventListener("click", () => {
+      const clickedText = fiberHeading[i].innerHTML;
+      console.log(clickedText);
+
+      fetch("/click", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: "abc",
+      })
+        .then((response) => {
+          if (response.ok) {
+            console.log("POST request sent successfully!");
+          } else {
+            console.error("Error sending POST request:", response.status);
+          }
+        })
+        .catch((error) => {
+          console.error("Error sending POST request:", error);
+        });
     });
+  }
 
-
-    ///////////////////////////
-
-        // Get the H1 element
-        const fiberHeading = document.querySelectorAll('.fiber-heading');
-
-        // Add a click event listener to the H1
-        for(let i =0;i<fiberHeading.length;i++){
-            fiberHeading[i].addEventListener('click', () => {
-                const clickedText = fiberHeading[i].innerHTML;
-                console.log(clickedText);
-                // Send a POST request to the backend with the clicked text
-                // fetch('/click', {
-                //     method: 'POST',
-                //     headers: {
-                //       'Content-Type': 'text/plain'
-                //     },
-                //     body: clickedText
-                //   });
-
-
-                  fetch('/click', {
-                    method: 'POST',
-                    headers: {
-                      'Content-Type': 'application/json'
-                    },
-                    body: "abc"
-                  })
-                    .then(response => {
-                      if (response.ok) {
-                        console.log('POST request sent successfully!');
-                      } else {
-                        console.error('Error sending POST request:', response.status);
-                      }
-                    })
-                    .catch(error => {
-                      console.error('Error sending POST request:', error);
-                    });
-
-
-              });
-        };
-
-    ///////////////////////////
-
+  ///////////////////////////
 }
--- server.js
+++ server.js
@@ -1,61 +1,74 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
 
 ///////////////////////// GET /////////////////////////
 
-let desList_cotton = [{
-    des:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
-    count:+56},{
-    des:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
-    count:+56},{
-    des:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
-    count:+56}
+let desList_cotton = [
+  {
+    des: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    count: +56,
+  },
+  {
+    des: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    count: +56,
+  },
+  {
+    des: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    count: +56,
+  },
 ];
 
-
-let desList_silk = [{
-    des:"It is a long established fact that",
-    count:+56},{
-    des:"It is a long established fact that",
-    count:+56},{
-    des:"It is a long established fact that",
-    count:+56}
+let desList_silk = [
+  {
+    des: "It is a long established fact that",
+    count: +56,
+  },
+  {
+    des: "It is a long established fact that",
+    count: +56,
+  },
+  {
+    des: "It is a long established fact that",
+    count: +56,
+  },
 ];
 
-let typeOfFiber = [{
-    fiberType:"Cotton",
-    count:156},{
-        fiberType:"Silk",
-    count:299
-    }]; 
-
-
+let typeOfFiber = [
+  {
+    fiberType: "Cotton",
+    count: 156,
+  },
+  {
+    fiberType: "Silk",
+    count: 299,
+  },
+];
 
 ///////////////////////// GET /////////////////////////
 
-
-app.get('/', (req, res) =>{
-    res.render('index',{typeOfFiber:typeOfFiber});
+app.get("/", (req, res) => {
+  res.render("index", { typeOfFiber: typeOfFiber });
 });
 
-app.post('/click', (req, res) => {
-    console.log(req.body);
-    // res.redirect('/details');
-    res.send('ok');
+app.post("/click", (req, res) => {
+  console.log("click request body", req.body);
+  // res.redirect('/details');
+  res.send("ok");
 });
 
-app.get('/details', (req, res) =>{
-    res.render('details',{desList_cotton:desList_cotton});
+app.get("/details", (req, res) => {
+  res.render("details", { desList_cotton: desList_cotton });
 });
 
-app.post('/', (req, res) => {
-    // console.log(req.body);
-    const { des, count } = req.body;
-    desList_cotton.push({ des, count });
-    res.redirect('details');
+app.post("/", (req, res) => {
+  // console.log(req.body);
+  const { des, count } = req.body;
+  desList_cotton.push({ des, count });
+  res.redirect("details");
 });
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, () => console.log("Server running on port 3000"));
